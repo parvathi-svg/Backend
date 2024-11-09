67fd6138199e5607502729336f7f0673f29cd141
cparvathi <cparvathiparvathi@gmail.com>
2024-11-09 10:49:54 +0530

    deploy my backend

---
+++ backend/test.js
@@ -0,0 +1,20 @@
+const express=require("express")
+const MongoClient=require("mongoose");
+const dotEnv=require("dotenv");
+const bodyParser=require("body-parser")
+const app=express();
+
+dotEnv.config();
+const PORT=process.env.PORT || 5555;
+
+MongoClient.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDB Atlas Cloud connected successfully")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+app.listen(PORT,()=>{
+    console.log(`My port is running on ${PORT} number`)
+})
